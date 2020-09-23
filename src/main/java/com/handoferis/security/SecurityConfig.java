b86797edd980356daf7c91b0c9413e89f1f1b33f
Your Name <you@example.com>
2020-09-23 10:19:46 -0400

    updating sec config to debug redirect issue

--- src/main/java/com/handoferis/security/SecurityConfig.java
+++ src/main/java/com/handoferis/security/SecurityConfig.java
@@ -44,14 +44,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //http.oauth2ResourceServer().jwt();
         //allows preflight headers which is fine
         http
-                .cors()
-                .and()
+                //.cors()
+                //.and()
                 .authorizeRequests()
-                .mvcMatchers("/api/user/test").permitAll()
+                //.mvcMatchers("/api/user/test").permitAll()
                 //.mvcMatchers("/api/user/test-private").authenticated()
                 //.mvcMatchers("/api/admin/uploadsong").hasAuthority("SCOPE_upload:music")
                 //.mvcMatchers("/api/admin/uploadsong").hasRole("ROLE_")
-                //.mvcMatchers("/api/admin/retrievesongs").hasAuthority("SCOPE_upload:music")
+                .mvcMatchers("/api/admin/retrievesongs").hasAuthority("SCOPE_upload:music")
 //                .and()
 //                .x509()
 //                .and()
